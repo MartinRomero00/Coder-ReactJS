06bcd1e73e97374c2a5d13ee21682be6ae6c43fa
Martin Romero <martinromanromero@gmail.com>
2023-03-18 19:51:11 -0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,19 @@
-import NavBar from "./components/NavBar/NavBar"
-import { useEffect, useState } from "react"
-import axios from "axios"
-import { Route, Routes } from "react-router-dom"
-import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
-import Categorys from "./components/categorias"
-import CardItem from "./components/itemDetail"
-import LowRate from "./components/lowRate"
-import HightRate from "./components/hightRate"
+import Header from "./components/Header"
+
 
 
 
 function App() {
 
-  const [products, setProducts] = useState([])
 
-  const getProducts = () => {
-    axios.get("https://fakestoreapi.com/products")
-    .then(res => {
-        setProducts(res.data)
-      })
-    .catch(err => {
-        console.log(err)
-      })
-  };
-
-  useEffect(() => {
-    getProducts();
-  }, [])
 
 
   
   return (
-    <div>
-    <NavBar />
-    <Categorys />
-    <Routes>
-    <Route path="/" element={<ItemListContainer productos={products}/>} />
-    <Route path="/item/:id" element={<CardItem productos={products}/>}/> 
-    <Route path="/lowrate" element={<LowRate productos={products}/>}/> 
-    <Route path="/hightrate" element={<HightRate productos={products}/>}/> 
-    </Routes> 
-    </div>
+    <>
+      <Header/> 
+      
+    </>
 
   )
 }
